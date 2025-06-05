585fb72d2c0375a21d2e71d9071ccc9963e4708f
xhd2015 <xhd2015@gmail.com>
2025-06-05 15:34:04 +0800

    update go.mod

--- tools/go/go_tools.go
+++ tools/go/go_tools.go
@@ -56,7 +56,7 @@ func HandleRun(args []string) error {
 	var hasGCflags bool
 	for i := 0; i < n; i++ {
 		arg := args[i]
-		if arg == "--debug" {
+		if arg == "--debug" || arg == "-debug" {
 			debug = true
 			continue
 		}
